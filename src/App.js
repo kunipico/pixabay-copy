e03e805cb73e17ff5667982a9278e082ca9dee3a
kunihiko <127746548+kunipico@users.noreply.github.com>
2024-04-14 22:38:21 +0900

    [Create]reactを使用して画像検索アプリを作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import ImageGallery from './ImageGallery';
 
 function App() {
+  const [fetchData, setFetchData] = useState([])
+  const ref = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(ref.current.value)
+    
+    // API URL
+    const endpointURL = 
+      `https://pixabay.com/api/?key=43385307-62aaa7aff1ec0a25276441c07&q=${ref.current.value}&image_type=photo`
+    // APIを叩くデータフェッチング　　fetch or axios
+    fetch(endpointURL)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      setFetchData(data.hits)
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='Search Image' ref={ref} />
+      </form>
+      <ImageGallery fetchData={fetchData} />
     </div>
   );
 }
